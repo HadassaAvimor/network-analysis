e58017cccfcbb051232273f02d9777bfc970e4ef
HadassaAvimor <h0533185978@gmail.com>
2023-07-24 16:25:55 +0300

    update main_controller

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,11 +1,5 @@
-from requests import Request
-
 from fastapi import FastAPI
 import uvicorn
-
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-import requests
 
 app = FastAPI()
 
