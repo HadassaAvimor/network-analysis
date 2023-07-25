88f64a9fb53fa8cf77625c85cd95b2648e977dfd
HadassaAvimor <h0533185978@gmail.com>
2023-07-25 12:21:58 +0300

    start with DB_connection

---
+++ models/DB_connection.py
@@ -0,0 +1,13 @@
+import pymysql.cursors
+
+connection = pymysql.connect(
+    host="http://sql6.freemysqlhosting.net/",
+    user="sql6635129",
+    password="UD9PIExeur",
+    db="sql6635129",
+    charset="utf8",
+    cursorclass=pymysql.cursors.DictCursor,
+)
+
+if connection.open:
+    print("the connection is opened")
