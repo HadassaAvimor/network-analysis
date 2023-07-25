177f6c6cc30c26c0635e7373168fb7823a81d8c7
HadassaAvimor <h0533185978@gmail.com>
2023-07-25 17:15:12 +0300

    add few comments

--- models/capture_reader.py
+++ models/capture_reader.py
@@ -3,6 +3,7 @@ def extract_network_information(capture_file):
     """
     A function that receives a cap file and returns a dictionary that contains information of the packets.
     :param capture_file: capture file to analyze
-    :return: network information :  Dict[packets, Dict[detail : information]]
+    :return: network information :  list of packets : List[Dict[detail : information]]
     """
+    pass
 
