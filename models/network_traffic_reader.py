2be0901a8df38fb0477655fb458b47382c48db4d
HadassaAvimor <h0533185978@gmail.com>
2023-07-25 10:43:39 +0300

    add comments

--- models/network_traffic_reader.py
+++ models/network_traffic_reader.py
@@ -1,19 +1,7 @@
-from abc import abstractmethod
-import abc
-
-
-class NetworkTrafficReader(abc):
-    @staticmethod
-    @abstractmethod
-    def get_devices_connections(capture_file):
-        """
-        A function that receives a cap file and returns a list of connections between devices.
-        :param capture_file: capture file to analyze
-        :return: devices connections : list of tuples
-        """
-        pass
-
-    @staticmethod
-    @abstractmethod
-    def extract_network_details(self):
-        pass
+def extract_network_information(capture_file):
+    """
+    A function that receives a cap file and returns a dictionary that contains information of the packets.
+    :param capture_file: capture file to analyze
+    :return: network information :  Dict[packets, Dict[detail : information]]
+    """
+    pass
--- models/pcap_reader.py
+++
@@ -1,5 +0,0 @@
-import network_traffic_reader
-
-
-class PcapReader(network_traffic_reader):
-    pass
