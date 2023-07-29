86edb60f96ccef11e3993457b2dab1f9f3e333ce
HadassaAvimor <h0533185978@gmail.com>
2023-07-29 22:19:58 +0300

    add capture file parser

--- models/capture_file_parser.py
+++ models/capture_file_parser.py
@@ -3,6 +3,11 @@ from scapy.utils import rdpcap
 
 
 def get_capture_packets_from_pcap(pcap_file):
+    """
+    A function that extract the packets from a pcap file.
+    :param pcap_file: pcap file to extract from.
+    :return: List[Packet]
+    """
     file_content = BytesIO(pcap_file)
     packets = rdpcap(file_content)
     return packets
