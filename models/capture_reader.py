669292f53f3c68e4d0f3ba101bc6aecf70874996
HadassaAvimor <h0533185978@gmail.com>
2023-07-29 22:32:40 +0300

    add branch capture_reader

--- models/capture_reader.py
+++ models/capture_reader.py
@@ -1,9 +1,32 @@
+from models import capture_file_parser
+
 
 def extract_network_information(capture_file):
     """
     A function that receives a cap file and returns a dictionary that contains information of the packets.
     :param capture_file: capture file to analyze
-    :return: network information :  list of packets : List[Dict[detail : information]]
+    :return: network information : List[Dict[detail : information]]
     """
-    pass
+    network = file_classification(capture_file)
+    return network
+
+
+def file_classification(capture_file):
+    """
+    A function that classifies the capture file according its type, and sends to the suitable processing for it.
+    :param capture_file: capture file to classify.
+    :return: List[Packet]
+    """
+    extension = capture_file.filename.split(".")[-1]
+    match extension:
+        case 'pcap':
+            return capture_file_parser.get_capture_packets_from_pcap(capture_file.file.read())
+        case 'pcapng':
+            return capture_file_parser.get_capture_packets_from_pcap(capture_file.file.read())
+        case 'cap':
+            pass
+        case 'erf':
+            pass
+    raise ValueError("Unsupported file extension: {}".format(extension))
+
 
--- models/network_analyze.py
+++ models/network_analyze.py
@@ -1,17 +1,21 @@
+from models import capture_reader
+
+
 def capture_analyze(capture_file):
     """
-    A function that performs analysis on a cap file.
+    A function that performs analysis of a network from a cap file.
     :param capture_file: file to analyze.
-    :return: network - Dict[network_detail : detail]
+    :return: Dict[network_detail : detail]
     """
-    pass
+    packets = capture_reader.extract_network_information(capture_file)
+    return packets
 
 
 def normal_packets_to_analyze(packets_list):
     """
     A function that normalizes the packet data to fit the device_analyze.
-    :param packets_list: packets list to normal.
-    :return: list of Dict[traffic_detail, detail]
+    :param packets_list: List[Packet]
+    :return: List[Dict[traffic_detail, detail]]
     """
     pass
 
