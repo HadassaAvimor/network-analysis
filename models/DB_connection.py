6ef9af9149813377a8543df04b2c253b01edce3a
HadassaAvimor <h0533185978@gmail.com>
2023-07-30 09:37:35 +0300

    change DB variebles to envirement variables.

--- models/DB_connection.py
+++ models/DB_connection.py
@@ -1,10 +1,15 @@
+import os
+
 import pymysql.cursors
+from dotenv import load_dotenv
+
+load_dotenv()
 
 connection = pymysql.connect(
     host="sql6.freemysqlhosting.net",
-    user="sql6635129",
-    password="UD9PIExeur",
-    db="sql6635129",
+    user=os.environ["DB_USERNAME"],
+    password=os.environ["DB_PASSWORD"],
+    db=os.environ["DB"],
     charset="utf8",
     cursorclass=pymysql.cursors.DictCursor,
 )
