86320285e8e2dddafd8b7f3674d309594f22145b
estiZukerman <hand2b20@mbjcomp.org.il>
2023-07-30 15:43:12 +0300

    add_row to db

--- models/DB_connection.py
+++ models/DB_connection.py
@@ -14,7 +14,7 @@ def create_server_connection():
             host="sql6.freemysqlhosting.net",
             user=os.environ["DB_USERNAME"],
             password=os.environ["DB_PASSWORD"],
-            db=os.environ["DB"],
+            db=os.environ["DB"]
         )
         print("MySQL Database connection successful")
     except Error as err:
@@ -23,4 +23,25 @@ def create_server_connection():
 
 
 db_connection = create_server_connection()
+
+
+def insert_row(table_name, values):
+    """
+  Inserts a row into a table in the database.
+  Args: table_name: The name of the table to insert the row into.
+        values: A dict of values to insert into the table.
+  Returns: the new row
+  """
+    cursor = db_connection.cursor()
+    # Create the SQL query.
+    query = "INSERT INTO %s (%s) VALUES (%s)" % (
+        table_name,
+        ",".join(values.keys()),
+        ",".join("%s" for _ in values.values())
+    )
+    cursor.execute(query, tuple(values.values()))
+    db_connection.commit()
+    db_connection.close()
+    return cursor.lastrowid
+
 
