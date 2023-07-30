bbdf26e812c2aaa520de18aa75285dcc9c8dd6d1
HadassaAvimor <117533373+HadassaAvimor@users.noreply.github.com>
2023-07-30 09:58:12 +0300

    Update network_controller.py

--- controllers/network_controller.py
+++ controllers/network_controller.py
@@ -33,7 +33,7 @@ async def get_network_by_id(network_id):
 @networks_router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_network(capture_file: UploadFile = File(...),
                          client_id: str = Form(...),
-                         data_taken: str = Form(...),
+                         date_taken: str = Form(...),
                          location_name: str = Form(...),):
     a = network_analyze.capture_analyze(capture_file)
     return {"packets": a, "client_id": client_id}
